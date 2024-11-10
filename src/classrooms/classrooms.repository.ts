928579577f0216b4a83bf6e6c353ae7d0b4562db
tareq-mahmood <tareq.mahmood@sazim.io>
2024-11-10 16:52:45 +0600

    feat(ST-28): teacher can create classroom (#30)
    * feat(#14): configured entities for database integration
    
    * feat(#10): user registration and code generator added
    
    * fix: minor changes on entity for typesafety
    
    * feat(st-14): entities created according ERD, removed irrelevant entities
    
    * feat(register): profile based registration added, unique code script fixed
    
    * refactor(st-10): changes made according to suggestions and registration
    
    * feat(ST-26): added single profile endpoint with jwt guard
    
    * chore: updated gitignore for storing production script files
    
    * refactor: jwtauthguard misplaced due to profile creation changes
    
    * feat(ST-28): teacher can create classroom
    
    * feat(ST-28): added dayjs as dependency
    
    * refactor(ST-28): refactored time based calculations using dayjs
    
    * chore: ran prettier,lint and generated successful build

---
+++ src/classrooms/classrooms.repository.ts
@@ -0,0 +1,70 @@
+import { ForbiddenException, Injectable } from "@nestjs/common";
+
+import { EntityManager } from "@mikro-orm/postgresql";
+
+import dayjs from "dayjs";
+
+import { Classroom } from "@/common/entities/classroom.entity";
+import { UserProfile } from "@/common/entities/user-profiles.entity";
+import { EUserRole } from "@/common/enums/roles.enums";
+
+import { CreateClassroomDto } from "./dto/create-classroom.dto";
+
+@Injectable()
+export class ClassroomRepository {
+  constructor(private readonly em: EntityManager) {}
+
+  async createClassroom(
+    createClassroomDto: CreateClassroomDto,
+    teacherId: number,
+  ): Promise<Classroom> {
+    const teacher = await this.em.findOne(UserProfile, teacherId);
+    const { title, subject, classTime, daysOfTheWeek } = createClassroomDto;
+    if (!teacher) {
+      throw new Error("Teacher not found");
+    }
+
+    if (teacher.role.name !== EUserRole.TEACHER) {
+      throw new ForbiddenException("Only teachers can create classrooms");
+    }
+
+    const [hours, minutes] = classTime.split(":");
+    const timeAsDate = dayjs()
+      .set("hour", Number(hours))
+      .set("minute", Number(minutes))
+      .second(0)
+      .millisecond(0)
+      .toDate();
+
+    const classroom = new Classroom();
+    classroom.title = title;
+    classroom.subject = subject;
+    classroom.classTime = timeAsDate;
+    classroom.daysOfTheWeek = daysOfTheWeek;
+    classroom.teacher = teacher;
+
+    await this.em.persistAndFlush(classroom);
+    return classroom;
+  }
+
+  async getClassroomById(id: number) {
+    const classRoom = await this.em.findOne(Classroom, id);
+    return classRoom;
+  }
+
+  async getClassrooms() {
+    const classRoom = await this.em.find(Classroom, {});
+    return classRoom;
+  }
+
+  async updateClassroom(classroom: Classroom) {
+    await this.em.persistAndFlush(classroom);
+    return classroom;
+  }
+
+  async deleteClassroom(id: number) {
+    const classroom = await this.em.findOne(Classroom, id);
+    await this.em.nativeDelete(Classroom, id);
+    return classroom;
+  }
+}

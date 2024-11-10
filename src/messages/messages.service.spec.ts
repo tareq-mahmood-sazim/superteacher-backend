f5fb1cc3f459c4a55f8510360768fe943f01fb66
tareq-mahmood-1-sazim <tareq.mahmood@sazim.io>
2024-11-10 16:36:16 +0600

    test(ST-49): added test suites for service and controllers

--- src/messages/messages.service.spec.ts
+++ src/messages/messages.service.spec.ts
@@ -1,19 +1,39 @@
 import { Test, TestingModule } from "@nestjs/testing";
 
+import { MessagesRepository } from "@/websocket-example/websocket-example.repository";
+
 import { MessagesService } from "./messages.service";
 
 describe("MessagesService", () => {
   let service: MessagesService;
+  let repository: MessagesRepository;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [MessagesService],
+      providers: [
+        MessagesService,
+        {
+          provide: MessagesRepository,
+          useValue: {
+            getMessageByClassroom: jest.fn().mockResolvedValue([]),
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<MessagesService>(MessagesService);
+    repository = module.get<MessagesRepository>(MessagesRepository);
   });
 
   it("should be defined", () => {
     expect(service).toBeDefined();
+  });
+
+  describe("findOne", () => {
+    it("should call getMessageByClassroom with the correct id", async () => {
+      const id = 1;
+      await service.findOne(id);
+      expect(repository.getMessageByClassroom).toHaveBeenCalledWith(id);
+    });
   });
 });
